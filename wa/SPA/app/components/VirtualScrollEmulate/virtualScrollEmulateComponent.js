77793f1a48ec1f6406337177928527bb304ca684
JS4lcaEq <condorrr@gmail.com>
2018-01-11 21:47:51 +0300

    rc2.5 ui + route

--- wa/SPA/app/components/VirtualScrollEmulate/virtualScrollEmulateComponent.js
+++ wa/SPA/app/components/VirtualScrollEmulate/virtualScrollEmulateComponent.js
@@ -38,46 +38,7 @@
 
         methods: {
 
-            onScroll: function (event) {
-                var cScroll = event.target.scrollTop;
-                var diff = cScroll - _curr.scroll.prev;
-                _addStat(diff);
-                var self = this;
 
-
-                if (!_curr.isFastScroll) {
-                    
-
-                    if (cScroll > _curr.scroll.max) {
-                        _ws.Up();
-                        _ws.Up();
-                        self.wndw = _ws.Get();
-                        self.debug.index = _ws.GetIndexes().start;
-                        _curr.isSlowScroll = true;
-                        self.elements.scroll.jquery.get(0).scrollTop = _ws.GetIndexes().start * _curr.k; //_options.scroll.step;
-                        //event.target.scrollTop = cScroll - _options.scroll.step;
-                        event.target.scrollTop = _options.scroll.base;
-                    }
-
-                    if (cScroll < _curr.scroll.min) {
-                        _ws.Down();
-                        _ws.Down();
-                        self.debug.index = _ws.GetIndexes().start;
-                        self.wndw = _ws.Get();
-                        if (_ws.GetIndexes().start > 0) {
-                            
-                            _curr.isSlowScroll = true;
-                            self.elements.scroll.jquery.get(0).scrollTop = _ws.GetIndexes().start * _curr.k; // _options.scroll.step;
-                            //event.target.scrollTop = cScroll + _options.scroll.step;
-                            event.target.scrollTop = _options.scroll.base;
-                        }
-                    }
-                }
-
-                _curr.isFastScroll = false;
-                _curr.scroll.prev = cScroll;
-                return true;
-            },
             onFastScroll: function (event) {
                 var self = this;
 
@@ -85,19 +46,19 @@
                     _curr.isFastScroll = true;
                     var scroll = event.target.scrollTop;
                     console.log("scroll = ", scroll);
-                    var startIndex = 0;
-                    if (scroll > 0) {
-                        startIndex = Math.round(scroll / _curr.k);
-                    }
+                    //var startIndex = 0;
+                    //if (scroll > 0) {
+                    //    startIndex = Math.round(scroll / _curr.k);
+                    //}
                  
-                    _ws.SetStartIndex(startIndex);
-                    self.debug.index = _ws.GetIndexes().start;
-                    self.wndw = _ws.Get();
-                    if (startIndex > 0) {
-                        self.elements.listBox.jquery.get(0).scrollTop = _options.scroll.base;
-                    } else {
-                        self.elements.listBox.jquery.get(0).scrollTop = 0;
-                    }
+                    //_ws.SetStartIndex(startIndex);
+                    //self.debug.index = _ws.GetIndexes().start;
+                    //self.wndw = _ws.Get();
+                    //if (startIndex > 0) {
+                    //    self.elements.listBox.jquery.get(0).scrollTop = _options.scroll.base;
+                    //} else {
+                    //    self.elements.listBox.jquery.get(0).scrollTop = 0;
+                    //}
                 }
 
                 _curr.isSlowScroll = false;
--- wa/SPA/app/vue.js
+++ wa/SPA/app/vue.js
@@ -1,19 +1,24 @@
-﻿(function () {
+﻿
+(function () {
+
+
+    ELEMENT.locale(ELEMENT.lang.ruRU);
+
 
     //var rs = app.services("RouteService").New("routes.json?t=" + Math.random());
     //console.log(rs);
 
     // 1. Определение используемых компонентов
     // Они могут быть импортированы из внешних файлов
-    const Foo = { template: '<div>foo</div>' };
-    const Bar = { template: '<div>bar{{data}}</div>', data: function () { return { data: "data" }; } };
+    var Foo = { template: '<div>foo</div>' };
+    var Bar = { template: '<div>bar{{data}}</div>', data: function () { return { data: "data" }; } };
 
     // 2. Определение путей
     // Каждый путь должен указывать на компонент
     // "Компонентом" может быть как созданный через `Vue.extend()`
     // полноценный конструктор, так и просто объект с настройками компонента
     // Вложенные пути будут рассмотрены далее.
-    const routes = [
+    var routes = [
         { path: '/', component: Vue.component('homePageComponent') },
         { path: '/foo', component: Vue.component('page1Component') },
         { path: '/bar', component: Vue.component('page1Component')},
@@ -24,7 +29,7 @@
 
     // 3. Создаём экземпляр роутера с опцией `routes`
     // Можно передать и другие опции, но пока не будем усложнять
-    const router = new VueRouter({
+    var router = new VueRouter({
         routes: routes
     });
 
@@ -37,7 +42,9 @@
             data: {
                 //routes: rs.get()
                 param: "param"
-            }
+            },
+            visible: false,
+            value1: null
         },
 
         methods: {
