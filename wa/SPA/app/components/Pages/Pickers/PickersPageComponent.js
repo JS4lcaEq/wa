a582a5190f4c0e9396a0e0050e697573f09cbb0d
Condor <condorrr@gmail.com>
2018-01-12 03:54:17 +0300

    rc2.8

---
+++ wa/SPA/app/components/Pages/Pickers/PickersPageComponent.js
@@ -0,0 +1,11 @@
+﻿(function () {
+
+    Vue.component('pickersPageComponent', {
+        template: app.services("TemplateService").Get("app/components/Pages/Pickers/PickersPageComponent.html"),
+        data: function() {
+            return { dataPickerModel: null };
+        }
+    });
+
+
+})();
